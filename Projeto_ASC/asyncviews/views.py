43eafe9bbbf4e74422af2c677f36e25e1ec5e326
marcelohiginoduarte <marcelihigino@gmail.com>
2024-06-11 13:31:23 -0300

    Initial commit

---
+++ Projeto_ASC/asyncviews/views.py
@@ -0,0 +1,13 @@
+import _asyncio
+from time import sleep
+import httpx
+from django.http import HttpResponse
+
+async def http_call_async():
+    for num in range(1, 6):
+        await asyncio.sleep(1)
+        print(num)
+    async with httpx.AsyncClient() as client:
+        r = await client.get("https://httpbin.org")
+        print(r)
+
